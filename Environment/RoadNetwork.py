11f04668a50716c75dba6c4b0373e3c98cd8f919
Mark Hruszczak <hruszcza@rhrk.uni-kl.de>
2020-04-09 00:15:43 +0200

    refactor(RoadNetwork): defined constructor partially

--- Environment/RoadNetwork.py
+++ Environment/RoadNetwork.py
@@ -1,9 +1,15 @@
-from Signals.I103_RoadNetwork import SignalsRoadNetwork
+from Signals.I103_RoadNetwork import *
 
 class RoadNetwork:
-    def __init__(self, road_category, lane_category, direction, id, previousIndices, nextIndices, leftIndex, rightIndex,rightMarkingIndex, leftMarkingIndex):
-
-        self.road_category = road_category
+    def __init__(self,
+                 timestamp_us: int,
+                 road_segments: List[RoadNetwork],
+                 lane_segments: List[LaneSegment],
+                 list_of_routes: List[Route],
+                 current_lane_index: int):
+        self.current_lane_index = lane_segments[current_lane_index]
+        # TODO: the road network consists not of a single road or lane, it consists of a set of road or lanes - please refactor the code
+        self.road_category = road_segments
         self.lane_category = lane_category
         self.direction = direction
         self.id = id
